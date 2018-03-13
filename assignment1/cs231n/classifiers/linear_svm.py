352167215d06c371edb52cbba1cebe53349648cf
Martolod Slaaf <mart@Martolods-MacBook-Pro.local>
2018-03-13 19:15:09 +0300

    Initial commit. Need to add dataset-getter

---
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -0,0 +1,83 @@
+import numpy as np
+from random import shuffle
+from past.builtins import xrange
+
+
+def svm_loss_naive(W, X, y, reg):
+    """
+    Structured SVM loss function, naive implementation (with loops).
+
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
+
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
+
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    dW = np.zeros(W.shape)  # initialize the gradient as zero
+
+    # compute the loss and the gradient
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    loss = 0.0
+
+    for i in xrange(num_train):
+        scores = X[i].dot(W)
+        correct_class_score = scores[y[i]]
+        for j in xrange(num_classes):
+            if j == y[i]:
+                continue
+            loss += max(0, 1 + scores[j] - correct_class_score)
+
+            if scores[j] > correct_class_score - 1:
+                dW[:, y[i]] -= X[i]
+                dW[:, j] += X[i]
+
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+    dW /= num_train
+
+    # Add regularization to the loss.
+    loss += reg * np.sum(W * W)
+    dW += 2 * reg * W
+
+    return loss, dW
+
+
+def svm_loss_vectorized(W, X, y, reg):
+    """
+    Structured SVM loss function, vectorized implementation.
+
+    Inputs and outputs are the same as svm_loss_naive.
+    """
+    dW = np.zeros(W.shape)  # initialize the gradient as zero
+    num_train = X.shape[0]
+
+    scores = X.dot(W)
+    correct_class_score = scores[np.arange(num_train), y]
+    dL = 1 + scores - correct_class_score[:, None]
+    dL[np.arange(num_train), y] = 0
+
+    indi_pictures, indi_classes = np.where(dL > 0)
+
+    for i, j in zip(indi_pictures, indi_classes):
+        dW[:, j] += X[i]
+        dW[:, y[i]] -= X[i]
+
+    loss = dL[dL > 0].sum()
+    loss /= num_train
+
+    dW /= num_train
+    dW += 2 * reg * W
+
+
+
+    return loss, dW
